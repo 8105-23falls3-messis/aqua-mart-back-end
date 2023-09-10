02f81e5a71a18f71ddeb0af43d69f275881df0f6
Jiehui Wang <magic.wong92@gmail.com>
2023-09-10 14:36:54 -0400

    project initial

---
+++ src/main/java/com/aqua/fall23g1/Fall23g1Application.java
@@ -0,0 +1,13 @@
+package com.aqua.fall23g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fall23g1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fall23g1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/aqua/fall23g1/Fall23g1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqua.fall23g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fall23g1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
