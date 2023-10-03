864cfc1aca3e0085febad2852aee7523260c41b9
Jiehui Wang <magic.wong92@gmail.com>
2023-10-03 19:05:22 -0400

    add error handle on login interface

--- src/main/java/com/aqua/fall23g1/config/LoginConfig.java
+++ src/main/java/com/aqua/fall23g1/config/LoginConfig.java
@@ -1,6 +1,9 @@
 package com.aqua.fall23g1.config;
 
-import com.aqua.fall23g1.interceptor.UserLoginInterceptor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
@@ -8,9 +11,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
+import com.aqua.fall23g1.interceptor.UserLoginInterceptor;
 
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
@@ -39,8 +40,8 @@ public class LoginConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         List<String> excludePath = new ArrayList<>();
-        excludePath.add("/register");
-        excludePath.add("/login");
+        excludePath.add("/user/register");
+        excludePath.add("/user/login");
         excludePath.add("/img/**");
         excludePath.add("/css/**");
         excludePath.add("/static/**");
--- src/main/java/com/aqua/fall23g1/controller/UserController.java
+++ src/main/java/com/aqua/fall23g1/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.aqua.fall23g1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +28,20 @@ public class UserController {
     public JSONObject login(LoginReq loginReq) {
         JSONObject loginRes = new JSONObject();
         User user = userService.getUserByLoginData(loginReq);
+        if (user == null) {
+            loginRes.put("status", "fail");
+            loginRes.put("msg", "email or password invalid");
+        }
         String signToken = TokenUtil.sign(user);
+        loginRes.put("status", "success");
         loginRes.put("token", signToken);
         return loginRes;
+    }
+
+    @GetMapping("/tokenTest")
+    public JSONObject tokenTest() {
+        JSONObject test = new JSONObject();
+        test.put("msg", "you get access to other interface");
+        return test;
     }
 }
